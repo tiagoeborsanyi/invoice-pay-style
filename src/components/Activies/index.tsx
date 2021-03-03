43dfb89a10b773641e45d0486fd8ea1a2f2cf7bf
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-03 18:48:45 -0300

    create lineChart component

--- src/components/Activies/index.tsx
+++ src/components/Activies/index.tsx
@@ -1,17 +1,80 @@
 import React from 'react';
+import { 
+  Area, 
+  ResponsiveContainer, 
+  AreaChart, 
+  XAxis, 
+  YAxis,
+  LineChart,
+  Line 
+} from 'recharts';
 
 import { 
   Container,
-  ATitle
+  ATitle,
+  ChartContainer,
+  ChartInfo
 } from './styles'
 
-const Activies = () => (
-  <Container>
-    <ATitle>
-        <h3>My Transactions</h3>
-        <button>This Week</button>
-      </ATitle>
-  </Container>
-);
+const Activies = () => {
+  const data = [
+    {
+      name: 'Mon',
+      uv: 20
+    },
+    {
+      name: 'Tue',
+      uv: 40,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Wed',
+      uv: 33
+    },
+    {
+      name: 'Thu',
+      uv: 28
+    },
+    {
+      name: 'Fri',
+      uv: 35
+    },
+    {
+      name: 'Sat',
+      uv: 42
+    },
+    {
+      name: 'Sun',
+      uv: 36
+    },
+  ];
+  return (
+    <Container>
+      <ATitle>
+          <h3>Activies</h3>
+          <button>This Week</button>
+        </ATitle>
+
+        <ChartContainer>
+          <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+              width={500}
+              height={400}
+              data={data}
+            >
+              <XAxis dataKey="name" stroke="#ccc" />
+              <YAxis width={23} stroke="#ccc" />
+              <Area type="monotone" dataKey="uv" strokeWidth={4} stroke="#FF9049" fill="#eee" />
+            </AreaChart>
+          </ResponsiveContainer>
+          <ChartInfo>
+            <h5>Total Spending</h5>
+            <span>2.800,00</span>
+          </ChartInfo>
+        </ChartContainer>
+    </Container>
+  )
+};
 
 export default Activies;
--- src/components/Activies/styles.ts
+++ src/components/Activies/styles.ts
@@ -6,7 +6,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  padding-right: 2rem;
+  padding-right: 5rem;
+  position: relative;
 `;
 
 export const ATitle = styled.div`
@@ -26,4 +27,34 @@ margin-bottom: 7rem;
     border-radius: 4px;
     font-size: 1.1rem;
   }
+`;
+
+export const ChartContainer = styled.div`
+  height: 65%;
+  width: 93%;
+  font-size: 600px;
+  position: absolute;
+  top: 71px;
+  left: 0px;
+`;
+
+export const ChartInfo = styled.div`
+  font-size: 3rem;
+  position: absolute;
+  top: 5%;
+  left: 45%;
+
+  > h5 {
+    font-size: 1rem;
+    font-weight: 600;
+    color: rgba(0,0,0,.7);
+    line-height: 0;
+  }
+
+  > span {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 2px;
+  }
+
 `;
